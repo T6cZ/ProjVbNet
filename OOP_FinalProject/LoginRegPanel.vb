f84dfb727e89be6ddc6cd703e7c98ba4a5002baa
Mandoe <145819011+makitaodes@users.noreply.github.com>
2024-12-10 15:35:54 +0800

    login show, password rec

--- OOP_FinalProject/LoginRegPanel.vb
+++ OOP_FinalProject/LoginRegPanel.vb
@@ -1,4 +1,5 @@
-﻿Imports MySql.Data.MySqlClient
+﻿Imports System.Web.UI.WebControls
+Imports MySql.Data.MySqlClient
 
 Public Class LoginRegPanel
 
@@ -8,7 +9,6 @@ Public Class LoginRegPanel
             {"@input", Login_email.Text},
             {"@password", Login_password.Text}
         }
-
 
         Dim userRow As DataRow = databaseConnection.GetDataRow(query, params)
 
@@ -45,7 +45,72 @@ Public Class LoginRegPanel
             Me.Hide()
 
         Else
-            Login_invalidusername.Text = "Invalid username, email, or password."
+            Login_invalidcredentials.Visible = True
+            login_errortimer.Enabled = True
         End If
+    End Sub
+
+    Private Sub login_showpass_CheckedChanged(sender As Object, e As EventArgs) Handles login_showpass.CheckedChanged
+        If login_showpass.Checked Then
+            Login_password.PasswordChar = ""
+        Else
+            Login_password.PasswordChar = "•"
+        End If
+    End Sub
+
+    Private Sub Login_errortimer_Tick(sender As Object, e As EventArgs) Handles login_errortimer.Tick
+        Login_invalidcredentials.Visible = False
+        login_errortimer.Enabled = False
+    End Sub
+
+    Private Sub Login_forgotpassword_Click(sender As Object, e As EventArgs) Handles Login_forgotpassword.Click
+        PasswordRecovery.BringToFront()
+        clear()
+    End Sub
+
+    Private Sub loginback_Click(sender As Object, e As EventArgs) Handles loginback.Click
+        LogIn.BringToFront()
+        clear()
+    End Sub
+
+    Private Sub clear()
+        Login_password.Text = ""
+        Login_email.Text = ""
+
+        rec_confirmpass.Text = ""
+        rec_newpass.Text = ""
+        rec_Email.Text = ""
+        rec_ID.Text = ""
+
+    End Sub
+
+
+
+    Private Sub rec_showpassword_CheckedChanged(sender As Object, e As EventArgs) Handles rec_showpassword.CheckedChanged
+        If rec_showpassword.Checked Then
+            rec_newpass.PasswordChar = ""
+        Else
+            rec_newpass.PasswordChar = "•"
+        End If
+    End Sub
+
+    Private Sub rec_showconfirmpassword_CheckedChanged(sender As Object, e As EventArgs) Handles rec_showconfirmpassword.CheckedChanged
+        If rec_showconfirmpassword.Checked Then
+            rec_confirmpass.PasswordChar = ""
+        Else
+            rec_confirmpass.PasswordChar = "•"
+        End If
+    End Sub
+
+    Private Sub PasswordRecovery_Paint(sender As Object, e As PaintEventArgs) Handles PasswordRecovery.Paint
+
+    End Sub
+
+    Private Sub Guna2Button2_Click(sender As Object, e As EventArgs)
+
+    End Sub
+
+    Private Sub invalid_IDaccount_Click(sender As Object, e As EventArgs) Handles invalid_IDaccount.Click
+
     End Sub
 End Class
